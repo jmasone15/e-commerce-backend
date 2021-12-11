e9970a2d6c6ebb34801f69dfafda62f4cb67207f
Jordan Masone <jordanmasone1@gmail.com>
2021-12-11 10:39:17 -0500

    Successfully returned all categories.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,11 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({}).then((categories) => {
+    res.status(200).json(categories);
+  }).catch((err) => {
+    res.status(500).json(err);
+  })
 });
 
 router.get('/:id', (req, res) => {
